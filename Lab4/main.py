fbf3c5be81b393cf2792925a1ef57b040064ebd9
yuanzm <1229084233@qq.com>
2014-11-28 17:07:08 +0800

    improve lab4

--- Lab4/main.py
+++ Lab4/main.py
@@ -8,22 +8,63 @@ from tornado.options import define, options
 define("port", default=8000, help="run on the given port", type=int)
 
 class info:
-	def __init__(self, name, section, card):
+	def __init__(self, name, section, card, cardType):
 		self.name = name
 		self.section = section
 		self.card = card
+		self.cardType = cardType
+	def infoString(self):
+		infoString = self.name + ";" + self.section  + ";" + self.card + ";" + self.cardType + "\n"
+		return infoString
+
+def writeToFile(path, content):
+	fs = open(path, "a")
+	fs.write(content)
+	fs.close()
+
+def readFromFile(path):
+	fs = open(path)
+	allLines = fs.read()
+	return allLines
+
+def Luhn(cardNumberString):
+	sum = 0
+	card = list(cardNumberString[::-1])
+	cardLength = len(card)
+
+	for index, number in enumerate(card):
+		if (cardLength - index - 1) % 2 == 1:
+			sum += int(number)
+		else:
+			doubleNum = int(number) * 2
+			if doubleNum < 10:
+				sum += doubleNum
+			else:
+				sum += doubleNum / 10 + doubleNum % 10
+	flag = True if sum % 10 == 0 else False
+	return flag
 
 class IndexHandler(tornado.web.RequestHandler):
 	def get(self):
+		print Luhn('4408041254369873')
 		self.render('buyagrade.html')
 
 class infoHandler(tornado.web.RequestHandler):
 	def post(self):
-		name = self.get_argument("name", "yuanzm")
-		section = self.get_argument("section", "MA")
-		card = self.get_argument("card", "1234123412341234")
-		infoMessage = info(name, section, card)
-		self.render('sucker.html', infoMessage=infoMessage)
+		name = self.get_argument("name", None)
+		section = self.get_argument("section", None)
+		card = self.get_argument("card", None)
+		cardType = self.get_argument('card-type', None)
+		filepath = os.path.join(os.path.dirname(__file__), "static/info/sucker.txt")
+		infoMessage = ''
+		if name == None or section == None or card == None or cardType == None:
+			err = True
+		else:
+			err = False
+			infoMessage = info(name, section, card, cardType)
+			writeToFile(filepath, infoMessage.infoString())
+		allLines = readFromFile(filepath)
+		self.render('sucker.html', infoMessage=infoMessage,allLines=allLines, err=err)
 
 if __name__ == '__main__':
 	tornado.options.parse_command_line()
