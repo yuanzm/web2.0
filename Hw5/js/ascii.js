f773e5e2548e3b4b8ea31e3689867d32c7df0359
yuanzm <1229084233@qq.com>
2014-12-10 23:49:09 +0800

    compress my code

--- Hw5/js/ascii.js
+++ Hw5/js/ascii.js
@@ -4,19 +4,17 @@
  */
 ;(function() {
 	'use strict';
-	var fontSize =  $("fontSize");
-	var sizeRadio = $A(fontSize.getElementsByTagName('input'));
-	var stratbutton = $("startButton");
-	var stopButton = $("stopButton");
-	var turboSpeed = $("turboSpeed");
-	var displayarea = $("displayarea");
-
+	var fontSize =  $("fontSize"),
+		sizeRadio = $A(fontSize.getElementsByTagName('input')),
+ 		startbutton = $("startButton"),
+		stopButton = $("stopButton"),
+		turboSpeed = $("turboSpeed"),
+		displayarea = $("displayarea");
 	window.onload = function() {
-		var ascii = new asciiAnimation(ANIMATIONS);
+		var ascii = new AsciiAnimation(ANIMATIONS);
 		ascii.init();
-		ascii.bindHandler();
 	}
-	function asciiAnimation(asciiArray) {
+	function AsciiAnimation(asciiArray) {
 		this.asciiArray = asciiArray;
 		this.asciiIndex = asciiArray[$F('selectButton')];
 		this.speed = 200;
@@ -24,68 +22,49 @@
 		this.nextFrame = null;
 		this.currentFrame = null;
 	}
-	asciiAnimation.prototype = {
-		constructor: asciiAnimation,
+	AsciiAnimation.prototype = {
+		constructor: AsciiAnimation,
 		fontArray: $w('7pt 12pt 24pt'),
 		init: function() {
 			stopButton.disabled = true;
+			this.bindHandler(this);
 		},
-		bindHandler: function() {
+		bindHandler: function(self) {
+			Event.observe(fontSize, 'click', self.changeFontSize.bindAsEventListener(self));
+			Event.observe(startbutton, 'click', self.startAnimation.bindAsEventListener(self));
+			Event.observe(stopButton, 'click', self.stopAnimation.bindAsEventListener(self));
+			turboSpeed.observe('click', function() { self.speed = $F('turboSpeed') === 'on'? 50:200;});
+		},
+		changeFontSize: function() {
 			var self = this;
-			fontSize.observe('click', function() {
-				self.changeFontSize(self);
-			});
-			stratbutton.observe('click', function() {
-				self.startAnimation(self);
-			});
-			stopButton.observe('click', function() {
-				self.stopAnimation(self);
-			});
-			turboSpeed.observe('click', function() {
-				var speed = $F('turboSpeed') === 'on'? 50:200;
-				self.turboAnimation(speed);
-			})
-		},
-		changeFontSize: function(self) {
 			sizeRadio.each(function(item, index) {
-				if(item.checked) {
-					displayarea.style.fontSize = self.fontArray[index];
-				}
+				if(item.checked) { displayarea.style.fontSize = self.fontArray[index];}
 			})
 		},
-		chooseAnimation: function(self) {
-			self.asciiIndex = self.asciiArray[$F('selectButton')];
-			self.currentFrame = self.asciiIndex.split("=====\n");
+		chooseAnimation: function() {
+			this.asciiIndex = this.asciiArray[$F('selectButton')];
+			this.currentFrame = this.asciiIndex.split("=====\n");
 		},
-		startAnimation: function(self) {
-			self.chooseAnimation(self);
-			if (self.currentFrame != '') {
-				self.actionAnimation(self, 0);
+		startAnimation: function() {
+			this.chooseAnimation();
+			if (this.currentFrame != '') {
+				this.actionAnimation(this, 0);
 				stopButton.disabled = false;
 				startButton.disabled = true;
 				selectButton.disabled = true;
 			}
 		},
-		stopAnimation: function(self) {
-			clearTimeout(self.nextFrame);
-			displayarea.value = self.asciiIndex;
+		stopAnimation: function() {
+			clearTimeout(this.nextFrame);
+			displayarea.value = this.asciiIndex;
 			startButton.disabled = false;
 			stopButton.disabled = true;
 			selectButton.disabled = false;
 		},
-		turboAnimation: function(speed) {
-			this.speed = speed;
-		},
 		actionAnimation: function(self, index) {
-			if (self.currentFrame.length > 0) {
-				if (index < self.currentFrame.length - 1) {
-					self.currentIndex = index + 1;
-				} else {
-					self.currentIndex = 0;
-				}
-				displayarea.value = self.currentFrame[index];
-				self.nextFrame = setTimeout(self.actionAnimation, self.speed, self, self.currentIndex);
-			}
+			self.currentIndex = index < self.currentFrame.length - 1?(index + 1):0;
+			displayarea.value = self.currentFrame[index];
+			self.nextFrame = setTimeout(self.actionAnimation, self.speed, self, self.currentIndex);
 		}
 	}
 })()
